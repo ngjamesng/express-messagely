3fca7594dc6ea9673866dcd05d9774c4b9c89040
ngjamesng <just.james.ng@gmail.com>
2020-02-17 12:02:37 -0800

    add authenticate, updatetimestamp, register, all, get static functions to the user class

--- models/user.js
+++ models/user.js
@@ -1,31 +1,59 @@
 /** User class for message.ly */
 
-
-
 /** User of the site. */
 
 class User {
-
-  /** register new user -- returns
+	/** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+	static async register({ username, password, first_name, last_name, phone }) {
+		const result = await db.query(
+			`INSERT INTO users (
+        username, password, first_name, last_name, phone
+      ) VALUES ($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`,
+			[ username, password, first_name, last_name, phone ]
+		);
+		return result.rows[0];
+	}
 
-  /** Authenticate: is this username/password valid? Returns boolean. */
+	/** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+	static async authenticate(username, password) {
+		const result = await db.query(
+			`SELECT username
+      FROM users
+      WHERE username = $1 AND password = $2`,
+			[ username, password ]
+		);
+		let u = result.rows[0];
+		return !!u;
+		// return Boolean(u);
+	}
 
-  /** Update last_login_at for user */
+	/** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+	static async updateLoginTimestamp(username) {
+		result = await db.query(
+			`UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1`,
+			[ username ]
+		);
+	}
 
-  /** All: basic info on all users:
+	/** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+	static async all() {
+		result = await db.query(`
+    SELECT username, first_name, last_name, phone
+    FROM users`);
+		return result.rows;
+	}
 
-  /** Get: get user by username
+	/** Get: get user by username
    *
    * returns {username,
    *          first_name,
@@ -34,9 +62,17 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+	static async get(username) {
+		results = await db.query(
+		`SELECT username, first_name, last_name, phone, join_at, last_login_at
+    FROM users
+    WHERE username = $1`,
+			[ username ]
+		);
+		return results.rows[0];
+	}
 
-  /** Return messages from this user.
+	/** Return messages from this user.
    *
    * [{id, to_user, body, sent_at, read_at}]
    *
@@ -44,9 +80,9 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+	static async messagesFrom(username) {}
 
-  /** Return messages to this user.
+	/** Return messages to this user.
    *
    * [{id, from_user, body, sent_at, read_at}]
    *
@@ -54,8 +90,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+	static async messagesTo(username) {}
 }
-
 
 module.exports = User;
